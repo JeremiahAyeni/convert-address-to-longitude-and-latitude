015afe8cfdfc42fca85214a4cd1fd458e834a599
JeremiahAyeni <38265526+JeremiahAyeni@users.noreply.github.com>
2019-12-03 11:02:50 +0100

    added a function to get longitude and latitude using Bing API
    use either google API or bing API to geocode - convert an address to its longitude and latitude

--- getLatLon.php
+++ getLatLon.php
@@ -22,4 +22,26 @@ function get_lat_long($address){
     return $latlon;
 }
 
+function get_lat_long_m($address){
+    $latlon = [];
+
+    $address = str_replace(" ", "%20", $address);
+    $userRegion='NG'; //for specific region you can specify else leave empty
+    $API_KEY= ''; //get key from google
+
+    $json = file_get_contents("http://dev.virtualearth.net/REST/v1/Locations?q=$address&ur=$userRegion&key=$API_KEY 
+    ");
+    $json = json_decode($json);
     
+    $lat = $json->{'resourceSets'}[0]->{'resources'}[0]->{'point'}->{'coordinates'}[0];
+    $long = $json->{'resourceSets'}[0]->{'resources'}[0]->{'point'}->{'coordinates'}[1];
+    
+    $latlon = $lat;
+    $latlon = $long;
+    
+    return $latlon;
+}
+
+
+
+
